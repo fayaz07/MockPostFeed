4002987a8fc9ec4edd6ce73e95e60d1889f0b26c
Mohammad Fayaz <fayazfz07@gmail.com>
2022-08-28 00:58:31 +0530

    add missing internet permission, API Key

--- app/src/main/java/com/mock/postfeed/data/network/Endpoints.kt
+++ app/src/main/java/com/mock/postfeed/data/network/Endpoints.kt
@@ -2,4 +2,5 @@ package com.mock.postfeed.data.network
 
 object Endpoints {
     const val baseUrl: String = "https://vixfl4ot4hof5l3qmrkik2v4ze0bbele.lambda-url.ap-south-1.on.aws"
+    const val API_KEY: String = "api123"
 }
--- app/src/main/java/com/mock/postfeed/data/network/PostFeedAPI.kt
+++ app/src/main/java/com/mock/postfeed/data/network/PostFeedAPI.kt
@@ -2,8 +2,9 @@ package com.mock.postfeed.data.network
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface PostFeedAPI {
     @GET("/")
-    suspend fun getPosts(): Response<List<PostModel>>
+    suspend fun getPosts(@Query("API_KEY") apiKey: String = Endpoints.API_KEY): Response<List<PostModel>>
 }
