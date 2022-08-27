d461feba1dd8b9264844b44c7a11f1eb08067453
Mohammad Fayaz <fayazfz07@gmail.com>
2022-08-28 01:58:30 +0530

    populate list with username and image

--- app/src/main/java/com/mock/postfeed/MainActivity.kt
+++ app/src/main/java/com/mock/postfeed/MainActivity.kt
@@ -3,17 +3,15 @@ package com.mock.postfeed
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.mock.postfeed.data.network.PostModel
 import com.mock.postfeed.ui.theme.MockPostFeedTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,23 +29,51 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun Content() {
         val mainViewModel = MainViewModel()
-        val state =  mainViewModel.state.collectAsState()
+        val state = mainViewModel.state.collectAsState()
 
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
-            Column {
-                Text(text = state.value.posts.toString())
-                Button(
-                    modifier = Modifier
-                        .height(48.dp)
-                        .width(200.dp),
-                    onClick = {
-                        mainViewModel.getPosts()
-                    }) {
-                    Text(text = "Get data")
+
+            if (state.value.posts.isEmpty()) {
+                Box {
+                    Button(
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .padding(16.dp)
+                            .height(48.dp)
+                            .width(200.dp),
+                        onClick = {
+                            mainViewModel.getPosts()
+                        }) {
+                        Text(text = "Get data")
+                    }
                 }
+            } else {
+                LazyColumn {
+                    items(state.value.posts.size) { index ->
+                        PostWidget(state.value.posts[index])
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun PostWidget(post: PostModel) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .height(220.dp)
+        ) {
+            Column {
+                Text(text = post.username, style = MaterialTheme.typography.h5)
+                AsyncImage(
+                    model = post.image,
+                    contentDescription = "Post by ${post.username}"
+                )
             }
         }
     }
