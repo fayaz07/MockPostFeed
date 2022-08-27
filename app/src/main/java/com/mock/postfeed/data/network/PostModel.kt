f09f51e2e63e20abe5ac83a067c1b8dd05256bc1
Mohammad Fayaz <fayazfz07@gmail.com>
2022-08-28 00:54:05 +0530

    add generate adapter qualifier for PostModel class, required kapt plugin for moshi

--- app/src/main/java/com/mock/postfeed/data/network/PostModel.kt
+++ app/src/main/java/com/mock/postfeed/data/network/PostModel.kt
@@ -1,5 +1,8 @@
 package com.mock.postfeed.data.network
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class PostModel(
     val id: Int,
     val createdAt: String,
