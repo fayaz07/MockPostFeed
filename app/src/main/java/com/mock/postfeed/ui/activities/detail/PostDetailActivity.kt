7fb9bfd3e231f40d951395017570c940dc67f33f
Mohammad Fayaz <fayazfz07@gmail.com>
2022-08-28 12:45:25 +0530

    Add Scaffold, topBar, navigation back to PostDetailActivity.kt

--- app/src/main/java/com/mock/postfeed/ui/activities/detail/PostDetailActivity.kt
+++ app/src/main/java/com/mock/postfeed/ui/activities/detail/PostDetailActivity.kt
@@ -1,12 +1,14 @@
 package com.mock.postfeed.ui.activities.detail
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 class PostDetailActivity : ComponentActivity() {
@@ -15,14 +17,27 @@ class PostDetailActivity : ComponentActivity() {
 
         val postId = intent.getIntExtra("postId", 0)
 
-        setContent {
+        setContent { Content(postId) }
+    }
 
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colors.background
-            ) {
-                Text("Data of post id: $postId will be shown here")
-            }
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    @Composable
+    fun Content(postId: Int) {
+        Scaffold(
+            topBar = {
+                TopAppBar(title = { Text(text = "Post detail") }, navigationIcon = {
+                    IconButton(
+                        onClick = { finish() }) {
+                        Icon(
+                            Icons.Filled.ArrowBack,
+                            "this will take us back to home"
+                        )
+                    }
+                })
+            },
+            modifier = Modifier.fillMaxSize(),
+        ) { _ ->
+            Text("Data of post id: $postId will be shown here")
         }
     }
 }
