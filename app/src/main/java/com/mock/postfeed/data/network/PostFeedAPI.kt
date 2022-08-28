b25b6dee43bc7112915ed7a06dc5dd576f52d2b5
Mohammad Fayaz <fayazfz07@gmail.com>
2022-08-28 12:54:35 +0530

    Add getPostById API declaration

--- app/src/main/java/com/mock/postfeed/data/network/PostFeedAPI.kt
+++ app/src/main/java/com/mock/postfeed/data/network/PostFeedAPI.kt
@@ -7,4 +7,10 @@ import retrofit2.http.Query
 interface PostFeedAPI {
     @GET("/")
     suspend fun getPosts(@Query("API_KEY") apiKey: String = Endpoints.API_KEY): Response<List<PostModel>>
+
+    @GET("/")
+    suspend fun getPostById(
+        @Query("API_KEY") apiKey: String = Endpoints.API_KEY,
+        @Query("id") id: Int = 0
+    ): Response<PostModel>
 }
